2ca43f538641147c5c70c619d09f4fde113925dd
mohamedmehdigara <garamehdi57@yahoo.com>
2023-08-19 11:34:47 +0100

    added files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Tic Tac Toe</h1>
+    <TicTacToeBoard :board="board" :makeMove="makeMove" />
+    <div v-if="winner">
+      <p>{{ winner }} wins!</p>
+      <button @click="resetGame">Play Again</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TicTacToeBoard from './components/TicTacToeBoard.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TicTacToeBoard,
+  },
+  data() {
+    return {
+      currentPlayer: 'X',
+      board: Array(9).fill(''),
+      winner: null,
+    };
+  },
+  methods: {
+    makeMove(index) {
+      if (!this.board[index] && !this.winner) {
+        this.board[index] = this.currentPlayer;
+        this.checkWinner();
+        this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+      }
+    },
+    checkWinner() {
+      // ... (rest of your checkWinner logic)
+    },
+    resetGame() {
+      this.currentPlayer = 'X';
+      this.board = Array(9).fill('');
+      this.winner = null;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Your styles here */
 </style>
---
+++ src/components/TicTacToeBoard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="board">
+    <div
+      v-for="(cell, index) in board"
+      :key="index"
+      class="cell"
+      @click="makeMove(index)"
+    >
+      {{ cell }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['board', 'makeMove'],
+};
+</script>
+
+<style scoped>
+.board {
+  display: grid;
+  grid-template-columns: repeat(3, 100px);
+  grid-gap: 5px;
+}
+
+.cell {
+  width: 100px;
+  height: 100px;
+  border: 1px solid #ccc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  cursor: pointer;
+}
+</style>
